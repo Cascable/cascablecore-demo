e0117abf9bc6f91001960ad14bf56b9b8eb4ed3c
Daniel Kennett <daniel@cascable.se>
2017-04-21 14:40:53 +0200

    Add loading indicator to filesystem screen

--- CascableCore iOS Demo (Obj-C)/FilesystemViewController.m
+++ CascableCore iOS Demo (Obj-C)/FilesystemViewController.m
@@ -12,6 +12,7 @@
 #import "Macros.h"
 
 @interface FilesystemViewController ()
+@property (strong, nonatomic) IBOutlet UIView *busyView;
 @property (nonatomic, readwrite) id <CBLCamera> camera;
 @property (nonatomic, readwrite) NSArray <id <CBLFileSystemItem>> *items;
 @end
@@ -22,8 +23,14 @@
 
     self.camera = camera;
 
+    [self loadViewIfNeeded];
+    [self showBusyOverlay];
+
     // CameraFileScanning is a helper class provided with this demo app that iterates through
     // the camera's directory structure and extracts files that the called might be interested in.
+    // You're welcome to use this in your own apps, but it may not provide the best UI
+    // experience — since it takes an all-or-nothing approach, if the camera has a lot of files
+    // your users will see nothing for a long time.
     CameraFileScanning *scanner = [CameraFileScanning sharedInstance];
 
     [scanner scanForFilesInCamera:self.camera matchingPredicate:^BOOL(id <CBLFileSystemItem> item) {
@@ -33,8 +40,59 @@
     } callback:^(NSArray <id <CBLFileSystemItem>> *items, NSError *error) {
         NSLog(@"%@: Camera scan got %@ matched items", THIS_FILE, @(items.count));
         self.items = items;
+        [self hideBusyOverlay];
         [self.tableView reloadData];
     }];
+}
+
+#pragma mark - Lifecycle
+
+-(void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Since our busy overlay is a floating view in the storyboard, it needs a little setup.
+    self.busyView.translatesAutoresizingMaskIntoConstraints = NO;
+    self.busyView.layer.cornerRadius = 20.0;
+    self.busyView.layer.masksToBounds = YES;
+
+    [self.busyView addConstraint:[NSLayoutConstraint constraintWithItem:self.busyView
+                                                              attribute:NSLayoutAttributeWidth
+                                                              relatedBy:NSLayoutRelationEqual
+                                                                 toItem:nil
+                                                              attribute:NSLayoutAttributeNotAnAttribute
+                                                             multiplier:1.0
+                                                               constant:200.0]];
+
+    [self.busyView addConstraint:[NSLayoutConstraint constraintWithItem:self.busyView
+                                                              attribute:NSLayoutAttributeHeight
+                                                              relatedBy:NSLayoutRelationEqual
+                                                                 toItem:nil
+                                                              attribute:NSLayoutAttributeNotAnAttribute
+                                                             multiplier:1.0
+                                                               constant:200.0]];
+}
+
+-(void)showBusyOverlay {
+    [self.busyView removeFromSuperview];
+    [self.view addSubview:self.busyView];
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.busyView
+                                                          attribute:NSLayoutAttributeCenterX
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterX
+                                                         multiplier:1.0 constant:0.0]];
+
+    [self.view addConstraint:[NSLayoutConstraint constraintWithItem:self.busyView
+                                                          attribute:NSLayoutAttributeCenterY
+                                                          relatedBy:NSLayoutRelationEqual
+                                                             toItem:self.view
+                                                          attribute:NSLayoutAttributeCenterY
+                                                         multiplier:1.0 constant:0.0]];
+}
+
+-(void)hideBusyOverlay {
+    [self.busyView removeFromSuperview];
 }
 
 #pragma mark - Table view data source
