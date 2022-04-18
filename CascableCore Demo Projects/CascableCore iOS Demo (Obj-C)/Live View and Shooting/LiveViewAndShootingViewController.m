400ae8f550ca7e544512abc1654b4f256fe9ea8d
Daniel Kennett <daniel@cascable.se>
2022-04-18 19:35:49 +0200

    Update to CascableCore 12

--- CascableCore Demo Projects/CascableCore iOS Demo (Obj-C)/Live View and Shooting/LiveViewAndShootingViewController.m
+++ CascableCore Demo Projects/CascableCore iOS Demo (Obj-C)/Live View and Shooting/LiveViewAndShootingViewController.m
@@ -13,16 +13,16 @@
 @interface LiveViewAndShootingViewController ()
 @property (nonatomic, readwrite) id <CBLCamera> camera;
 @property (weak, nonatomic) IBOutlet UIImageView *liveViewImageView;
-@property (nonatomic, strong) CBLCameraObserverToken *shotPreviewObserver;
+@property (nonatomic, strong) CBLCameraObserverToken *cameraInititatedTransferToken;
 @property (nonatomic, strong) UIImage *lastShotPreviewImage;
 @end
 
 @implementation LiveViewAndShootingViewController
 
 -(void)dealloc {
-    // Make sure we remove our shot preview observer so it doesn't get fired after we're deallocated!
-    if (self.shotPreviewObserver != nil) {
-        [self.camera removeShotPreviewObserverWithToken:self.shotPreviewObserver];
+    // Make sure we remove our camera-initiated transfer handler so it doesn't get fired after we're deallocated!
+    if (self.cameraInititatedTransferToken != nil) {
+        [self.camera removeCameraInitiatedTransferHandlerWithToken:self.cameraInititatedTransferToken];
     }
 
     // Turn off live view if it's running.
@@ -37,11 +37,12 @@
 
     self.camera = camera;
 
-    // In order to show previews of shots after the shutter has been fired, we need to register a shot preview observer.
+    // In order to show previews of shots after the shutter has been fired, we need to register a
+    // camera-initiated transfer handler.
     CBLWeakify(self);
-    self.shotPreviewObserver = [self.camera addShotPreviewObserver:^(id <CBLCameraShotPreviewDelivery> previewDelivery) {
+    self.cameraInititatedTransferToken = [self.camera addCameraInitiatedTransferHandler:^(id <CBLCameraInitiatedTransferRequest> req) {
         CBLStrongify(self);
-        [self handleShotPreview:previewDelivery];
+        [self handleCameraInitiatedTransferRequest:req];
     }];
 
     CBLCameraLiveViewFrameDelivery delivery = ^(id<CBLCameraLiveViewFrame> frame, dispatch_block_t completionHandler) {
@@ -88,35 +89,62 @@
     }];
 }
 
-#pragma mark - Shot Preview
+#pragma mark - Camera-Initiated Transfers
 
--(void)handleShotPreview:(id <CBLCameraShotPreviewDelivery>)previewDelivery {
+-(void)handleCameraInitiatedTransferRequest:(id <CBLCameraInitiatedTransferRequest>)request {
 
-    // Shot previews get delivered (on supported cameras) when a new preview is available. Previews will become invalid
-    // after an amount of time, so it's important to check they're still valid before fetching.
+    // Camera-initated transfer requests get sent by supported cameras when a new photo has been taken and the
+    // camera is presenting on opportunity for that photo to be transferred to a connected host. Transfer requests
+    // can become invalid after an amount of time, so it's important to check they're still valid before fetching them.
 
-    // Since fetching a preview can delay other commands, they're only fetched if you ask for them, which
-    // we do here 100% of the time.
+    // In some situations, this transfer may be the *only* destination of an image — for example, if the camera
+    // doesn't have a memory card present or is set to a "host only" image saving mode. If this is the case, the
+    // request's `-isOnlyDestinationForImage` property will be set to `YES`. These requests should be executed
+    // with the `CBLCameraInitiatedTransferRepresentationOriginal` representation to get the original image file
+    // and to avoid data loss. However, for this example we're going to ignore that and only care about previews.
 
-    if (!previewDelivery.isValid) {
-        NSLog(@"%@: Shot preview received, but it's invalid", THIS_FILE);
+    // Since executing a transfer can delay other commands, they're only executed if you ask for them, which we do
+    // here 100% of the time if a preview representation is available.
+
+    if (!request.isValid) {
+        NSLog(@"%@: Camera-initated transfer request received, but it's invalid", THIS_FILE);
         return;
     }
 
-    NSLog(@"%@: Fetching shot preview…", THIS_FILE);
+    if (![request canProvideRepresentation:CBLCameraInitiatedTransferRepresentationPreview]) {
+        NSLog(@"%@: Camera-initated transfer request received, but it can't provide a preview representation", THIS_FILE);
+        return;
+    }
 
-    [previewDelivery fetchShotPreview:^(NSData *sourceData, UIImage *preview, NSError *error) {
-        // sourceData is the raw image data as received from the camera, before any rotation etc. is applied.
-        // This can be useful if you want to apply your own tranformations to the image.
-        if (error != nil || preview == nil) {
-            NSLog(@"%@: Shot preview fetch failed with error: %@", THIS_FILE, error);
+    NSLog(@"%@: Fetching preview…", THIS_FILE);
+
+    // The `representations` parameter is a bitmask — we can request both the preview and original representations
+    // if we want. However, only requesting a preview allows CascableCore to optimise the request and transfer less
+    // data from the camera in certain circumstances – reducing the time taken.
+    [request executeTransferForRepresentations:CBLCameraInitiatedTransferRepresentationPreview
+                             completionHandler:^(id <CBLCameraInitiatedTransferResult> result, NSError *error) {
+
+        if (error != nil || result == nil) {
+            NSLog(@"%@: Camera-initiated transfer failed with error: %@", THIS_FILE, error);
             return;
         }
 
-        NSLog(@"%@: Shot preview fetch succeeded", THIS_FILE);
+        // At this point, the transfer from the camera is complete and we can use the result object to get at
+        // the transferred image representations. In this example, we just want to display a preview image
+        // on screen — for more advanced operations, we can write the result out to disk or get it as a raw
+        // data object.
 
-        self.lastShotPreviewImage = preview;
-        [self performSegueWithIdentifier:@"shotPreview" sender:self];
+        [result generatePreviewImage:^(UIImage *previewImage, NSError *error) {
+            // It's rare that we can fail at this point, but it _is_ possible — for example, if the source image
+            // is a RAW format we don't know how to handle yet.
+            if (previewImage == nil) {
+                NSLog(@"%@: Failed to generate preview image with error: %@", THIS_FILE, error);
+                return;
+            }
+
+            self.lastShotPreviewImage = previewImage;
+            [self performSegueWithIdentifier:@"shotPreview" sender:self];
+        }];
     }];
 }
 
--- CascableCore Demo Projects/CascableCore iOS Demo (Swift)/Live View and Shooting/LiveViewAndShootingViewController.swift
+++ CascableCore Demo Projects/CascableCore iOS Demo (Swift)/Live View and Shooting/LiveViewAndShootingViewController.swift
@@ -7,8 +7,8 @@ import Combine
 @objc(LiveViewAndShootingViewController) class LiveViewAndShootingViewController: UIViewController, CameraViewController {
 
     deinit {
-        // Make sure we remove our shot preview observer so it doesn't get fired after we're deallocated!
-        if let observer = shotPreviewObserver { camera?.removeShotPreviewObserver(with: observer) }
+        // Make sure we remove our camera-initiated transfer handler so it doesn't get fired after we're deallocated!
+        if let observer = cameraInititatedTransferToken { camera?.removeCameraInitiatedTransferHandler(with: observer) }
 
         // Turn off live view if it's running.
         stopLiveView()
@@ -22,9 +22,10 @@ import Combine
         self.camera = camera
         guard let camera = camera else { return }
 
-        // Shot previews let us preview shots with a reasonably high quality as they're taken.
-        shotPreviewObserver = camera.addShotPreviewObserver({ [weak self] delivery in
-            self?.handleShotPreview(delivery)
+        // In order to show previews of shots after the shutter has been fired, we need to register a
+        // camera-initiated transfer handler.
+        cameraInititatedTransferToken = camera.addCameraInitiatedTransferHandler({ [weak self] request in
+            self?.handleCameraInitiatedTransferRequest(request)
         })
 
         startLiveView(on: camera)
@@ -49,38 +50,62 @@ import Combine
 
     // MARK: - Shot Preview
 
-    var shotPreviewObserver: String?
+    var cameraInititatedTransferToken: String?
     var lastShotPreviewImage: UIImage?
 
-    func handleShotPreview(_ previewDelivery: ShotPreviewDelivery) {
+    func handleCameraInitiatedTransferRequest(_ request: CameraInitiatedTransferRequest) {
 
-        // Shot previews get delivered (on supported cameras) when a new preview is available. Previews will become invalid
-        // after an amount of time, so it's important to check they're still valid before fetching.
+         // Camera-initated transfer requests get sent by supported cameras when a new photo has been taken and the
+         // camera is presenting on opportunity for that photo to be transferred to a connected host. Transfer requests
+         // can become invalid after an amount of time, so it's important to check they're still valid before fetching them.
 
-        // Since fetching a preview can delay other commands, they're only fetched if you ask for them, which
-        // we do here 100% of the time.
+         // In some situations, this transfer may be the *only* destination of an image — for example, if the camera
+         // doesn't have a memory card present or is set to a "host only" image saving mode. If this is the case, the
+         // request's `-isOnlyDestinationForImage` property will be set to `YES`. These requests should be executed
+         // with the `CBLCameraInitiatedTransferRepresentationOriginal` representation to get the original image file
+         // and to avoid data loss. However, for this example we're going to ignore that and only care about previews.
 
-        guard previewDelivery.isValid else {
-            print("\(CurrentFileName()): Shot preview received, but it's invalid!")
+         // Since executing a transfer can delay other commands, they're only executed if you ask for them, which we do
+         // here 100% of the time if a preview representation is available.
+
+        guard request.isValid else {
+            print("\(CurrentFileName()): Camera-initated transfer request received, but it's invalid")
             return
         }
 
-        print("\(CurrentFileName()): Fetching shot preview…")
+        guard request.canProvide(.preview) else {
+            print("\(CurrentFileName()): Camera-initated transfer request received, but it can't provide a preview representation")
+            return
+        }
 
-        previewDelivery.fetchShotPreview { [weak self] sourceData, previewImage, error in
-            guard let self = self else { return }
+        print("\(CurrentFileName()): Fetching preview…")
 
-            // sourceData is the raw image data as received from the camera, before any rotation etc. is applied.
-            // This can be useful if you want to apply your own tranformations to the image.
+         // The `representations` parameter is an option set — we can request both the preview and original representations
+         // if we want. However, only requesting a preview allows CascableCore to optimise the request and transfer less
+         // data from the camera in certain circumstances – reducing the time taken.
 
-            guard error == nil, let preview = previewImage else {
-                print("\(CurrentFileName()): Shot preview fetch failed with error: \(error?.localizedDescription ?? "unknown")")
+        request.executeTransfer(for: .preview, completionQueue: .main) { result, error in
+            guard error == nil, let result = result else {
+                print("\(CurrentFileName()): Camera-initiated transfer failed with error: \(String(describing: error))")
                 return
             }
 
-            print("Shot preview fetch succeeded")
-            self.lastShotPreviewImage = preview
-            self.performSegue(withIdentifier: "shotPreview", sender: self)
+            // At this point, the transfer from the camera is complete and we can use the result object to get at
+            // the transferred image representations. In this example, we just want to display a preview image
+            // on screen — for more advanced operations, we can write the result out to disk or get it as a raw
+            // data object.
+
+            result.generatePreviewImage { [weak self] previewImage, previewError in
+                // It's rare that we can fail at this point, but it _is_ possible — for example, if the source image
+                // is a RAW format we don't know how to handle yet.
+                guard previewError == nil, let image = previewImage else {
+                    print("\(CurrentFileName()): Failed to generate preview image with error: \(String(describing: previewError))")
+                    return
+                }
+
+                self?.lastShotPreviewImage = image
+                self?.performSegue(withIdentifier: "shotPreview", sender: self)
+            }
         }
     }
 
